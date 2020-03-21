e8d28b7ce8f302133d3bb9a052ab19ecc1d76495
Leafhmy <954530701@qq.com>
2020-03-21 20:18:27 +0800

    ds

---
+++ Hello.java
@@ -0,0 +1,17 @@
+/**
+ * Hello
+ */
+import java.util.Arrays;
+public class Hello {
+
+    public static void main(String[] args) {
+        int[][] arr = {
+            {1,2,3,5},
+            {2,3,4},
+            {6,3,4,6,7}
+        };
+
+        System.out.println(Arrays.deepToString(arr));
+
+    }
+}
